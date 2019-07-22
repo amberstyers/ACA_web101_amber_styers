4ee336404e2e7b0eeabd39b39f4315481abafe2d
amberstyers <amberstyers@gmail.com>
2019-07-21 23:35:10 -0500

    tictac

---
+++ src/TicTacToe/tictactoe.js
@@ -0,0 +1,17 @@
+//in HTML
+//create 3*3 grid
+//in JS
+// unclick insert 'o' or 'x' depending on previous click
+//button was clicked
+//var player1, var player2 var previousClick
+console.log('hey')
+var box = document.querySelector('.box')
+var allBoxes = document.querySelectorAll('.box')
+console.log('allBoxes', allBoxes)
+function playTicTacToe(element) {
+  console.log('hey element', element)
+  element.innerText = 'o'
+}
+element.style.fontSize=60 +'px'
+element.style.background='pink'
+element.innerText = "X"
